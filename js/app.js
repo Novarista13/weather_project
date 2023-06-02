18dae10a7fa36ceeba5e08fcce70d3b808895ac3
Novarista13 <134473935+Novarista13@users.noreply.github.com>
2023-06-02 23:20:33 +0630

    changed weekdays and time format

--- js/app.js
+++ js/app.js
@@ -1,20 +1,33 @@
 let now = new Date();
 
 let days = [
-  "Sunday",
-  "Monday",
-  "Tuesday",
-  "Wednesday",
-  "Thursday",
-  "Friday",
-  "Saturaday",
+  "Sun",
+  "Mon",
+  "Tue",
+  "Wed",
+  "Thu",
+  "Fri",
+  "Sat",
 ];
+
 let day = days[now.getDay()];
 let hour = now.getHours();
 let minutes = now.getMinutes();
+if(hour < 10) {hour = `0${hour}`};
+if(minutes < 10) {minutes = `0${minutes}`};
 
 let today = document.querySelector("#default-time");
 today.innerHTML = `${day} ${hour}:${minutes}`;
+
+// For the week days
+for (let x = 1; x < 6; x++){
+let nextDay = document.querySelector(`.weather-day${x}`);
+ let weekDay = now.getDay() + x;
+ if(weekDay > 6) {
+  weekDay=x-2;
+ }
+ nextDay.innerHTML= days[weekDay];
+}
 
 // For the cities from the bar
 let city1 = document.querySelector("#yangon");
